c60a98d4e06a66420ffbae3e79c19b111bb4f1a6
Luka <lukhanyo95@gmail.com>
2021-02-11 16:35:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+The Odin Project Coding Exercise.
+
+This is a basic Tic Tac Toe, a game that you can play in your browser. My future plans with this project:
+
+1) Create at least two rounds of play
+2) Create a better interface that inputs players' names.(instead of using prompts)
+3) Apply media queries so the website can respond beautifuly to devices of varying widths.
+
---
+++ script.js
@@ -0,0 +1,131 @@
+
+
+
+
+const board = (() =>{
+const player1Name = prompt('Enter name: ');
+const player2Name = prompt('Enter name: ')
+const player1 = 'X';
+const player2 = 'O'
+const gameStatus = document.getElementById('text');
+const winMessage = () => `${gamePlay.currentPlayerName} has won!`;
+const drawMessage = () => 'It\'s a draw';
+
+return{player1,player2, gameStatus, winMessage,drawMessage, player1Name,player2Name}
+
+})();
+
+
+const gamePlay = (() =>{
+   
+    let currentPlayer = board.player1;
+    let currentPlayerName = board.player1Name;
+    let gameActive = true;
+    let gameState = ['','','','','','','','',''];
+
+    
+    
+    const CellClick = (clickedCellEvent) =>{
+        const clickedCell = clickedCellEvent.target;
+        const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index'));
+        
+        if (gameState[clickedCellIndex] !=='' || !gameActive){
+            return
+        }
+        CellPlayed(clickedCell, clickedCellIndex);
+        ResultValidation();
+        }
+
+
+    const PlayerChange = () =>{
+        if(currentPlayer === board.player1){
+            currentPlayer = board.player2;
+            currentPlayerName = board.player2Name;
+        }else{
+            currentPlayer = board.player1;
+            currentPlayerName = board.player1Name;
+        }
+    
+        board.gameStatus.textContent = playerTurn(); 
+    }
+
+
+    const RestartGame = () =>{
+        gameActive = true;
+        gameState = ['','','','','','','','',''];
+        board.gameStatus.textContent = playerTurn();
+        document.querySelectorAll('.grid-item').forEach(cell => cell.innerHTML = "");
+    }
+
+
+
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+
+    const ResultValidation = () =>{
+        let roundWon = false;
+        for(let i=0; i <=7; i++){
+            const winCondition = winningConditions[i];
+
+            let a = gameState[winCondition[0]];
+            let b = gameState[winCondition[1]];
+            let c = gameState[winCondition[2]];
+
+        if(a === '' || b ==='' || c === ''){
+            continue;
+        }
+
+        if(a === b && b === c){
+            roundWon = true;
+            break;
+        }
+
+         }
+
+        if(roundWon){
+            board.gameStatus.textContent = board.winMessage();
+            gameActive = false;
+            return;
+        }
+
+        let roundDraw = !gameState.includes('');
+
+        if (roundDraw){
+            gameStatus.textContent = board.drawMessage();
+            gameActive = false;
+            return;
+        }
+    PlayerChange();
+}
+
+const playerTurn = () => `It's ${currentPlayerName}'s turn`;
+
+    board.gameStatus.textContent = playerTurn();
+
+    const CellPlayed = (clickedCell, clickedCellIndex) =>{
+        gameState[clickedCellIndex] = currentPlayer;
+        clickedCell.textContent = currentPlayer;
+    
+    }
+
+
+document.querySelectorAll('.grid-item').forEach(cell => cell.addEventListener('click',CellClick));
+document.getElementById('btn').addEventListener('click',RestartGame);
+
+return{currentPlayer, currentPlayerName}
+
+})();
+
+
+
+
