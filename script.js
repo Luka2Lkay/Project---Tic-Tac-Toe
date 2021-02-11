1e737fbae407ff8c9d2ea2c6d7eaf8af68eabfdd
Luka <lukhanyo95@gmail.com>
2021-02-11 16:45:09 +0200

    second commit

--- script.js
+++ script.js
@@ -3,18 +3,15 @@
 
 
 const board = (() =>{
-const player1Name = prompt('Enter name: ');
-const player2Name = prompt('Enter name: ')
+const player1Name = prompt('Enter Player 1\'s name: ');
+const player2Name = prompt('Enter Player 2\'s name: ')
 const player1 = 'X';
-const player2 = 'O'
+const player2 = 'O';
 const gameStatus = document.getElementById('text');
 const winMessage = () => `${gamePlay.currentPlayerName} has won!`;
 const drawMessage = () => 'It\'s a draw';
-
 return{player1,player2, gameStatus, winMessage,drawMessage, player1Name,player2Name}
-
 })();
-
 
 const gamePlay = (() =>{
    
@@ -23,8 +20,6 @@ const gamePlay = (() =>{
     let gameActive = true;
     let gameState = ['','','','','','','','',''];
 
-    
-    
     const CellClick = (clickedCellEvent) =>{
         const clickedCell = clickedCellEvent.target;
         const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index'));
@@ -36,7 +31,6 @@ const gamePlay = (() =>{
         ResultValidation();
         }
 
-
     const PlayerChange = () =>{
         if(currentPlayer === board.player1){
             currentPlayer = board.player2;
@@ -45,10 +39,8 @@ const gamePlay = (() =>{
             currentPlayer = board.player1;
             currentPlayerName = board.player1Name;
         }
-    
-        board.gameStatus.textContent = playerTurn(); 
+    board.gameStatus.textContent = playerTurn(); 
     }
-
 
     const RestartGame = () =>{
         gameActive = true;
@@ -56,9 +48,6 @@ const gamePlay = (() =>{
         board.gameStatus.textContent = playerTurn();
         document.querySelectorAll('.grid-item').forEach(cell => cell.innerHTML = "");
     }
-
-
-
 
     const winningConditions = [
         [0, 1, 2],
@@ -71,7 +60,6 @@ const gamePlay = (() =>{
         [2, 4, 6]
     ];
 
-
     const ResultValidation = () =>{
         let roundWon = false;
         for(let i=0; i <=7; i++){
@@ -80,26 +68,21 @@ const gamePlay = (() =>{
             let a = gameState[winCondition[0]];
             let b = gameState[winCondition[1]];
             let c = gameState[winCondition[2]];
-
         if(a === '' || b ==='' || c === ''){
             continue;
         }
-
         if(a === b && b === c){
             roundWon = true;
             break;
         }
 
          }
-
         if(roundWon){
             board.gameStatus.textContent = board.winMessage();
             gameActive = false;
             return;
         }
-
         let roundDraw = !gameState.includes('');
-
         if (roundDraw){
             gameStatus.textContent = board.drawMessage();
             gameActive = false;
@@ -109,21 +92,14 @@ const gamePlay = (() =>{
 }
 
 const playerTurn = () => `It's ${currentPlayerName}'s turn`;
-
     board.gameStatus.textContent = playerTurn();
-
     const CellPlayed = (clickedCell, clickedCellIndex) =>{
         gameState[clickedCellIndex] = currentPlayer;
         clickedCell.textContent = currentPlayer;
-    
     }
-
-
 document.querySelectorAll('.grid-item').forEach(cell => cell.addEventListener('click',CellClick));
 document.getElementById('btn').addEventListener('click',RestartGame);
-
 return{currentPlayer, currentPlayerName}
-
 })();
 
 
